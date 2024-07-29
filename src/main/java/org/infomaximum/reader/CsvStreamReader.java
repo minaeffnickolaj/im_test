bd581d9c156d62f371aa124bff19aabb067f8183
Nickolai Minaev <minaev.36.ru@gmail.com>
2024-07-29 05:41:14 +0300

    Add dependency for CSV reading to gradle, add CsvStreamReader implementation of StreamReader

---
+++ src/main/java/org/infomaximum/reader/CsvStreamReader.java
@@ -0,0 +1,64 @@
+package org.infomaximum.reader;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.infomaximum.entities.Record;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+// AutoCloseable для автоматического закрытия/освобождения ресурсов по завершению работы
+public class CsvStreamReader implements StreamReader<Record>, AutoCloseable {
+    private final CSVReader reader;
+    private Iterator<String[]> iterator;
+
+    public CsvStreamReader(String filePath) throws CsvException {
+        try {
+            this.reader = new CSVReader(new FileReader(filePath));
+            this.iterator = reader.iterator();
+            if (iterator.hasNext()) {
+                iterator.next(); //пропустим заголовки
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new CsvException("Не удалось открыть файл!");
+        }
+    }
+
+    @Override
+    public Record readNext() throws NoSuchElementException {
+        if (!hasNext()) {
+            throw new NoSuchElementException("Файл вычитан!");
+        }
+        String[] row = iterator.next();
+        return rowToData(row);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    //переопределение close() из AutoCloseable
+    @Override
+    public void close() throws IOException {
+        reader.close();
+    }
+
+    private Record rowToData(String[] row) {
+        if (row == null || row.length < 4) { //проверяем на целостность
+            throw new IllegalArgumentException("Битая строка, недостаточно данных");
+        }
+        try{
+            String group = row[0];
+            String type = row[1];
+            long number = Long.parseLong(row[2]);
+            long weight = Long.parseLong(row[3]);
+            return new Record(group, type, number, weight);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Ошибка парсинга строки");
+        }
+    }
+}
